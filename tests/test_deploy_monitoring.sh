d6685469c6990db7dcb7ef681877d9fc5f4266a9
den <dennis.victorovich@gmail.com>
2019-07-01 14:21:58 +0300

    setup /etc/hosts for FQDN lookup required by collectd

--- tests/test_deploy_monitoring.sh
+++ tests/test_deploy_monitoring.sh
@@ -9,5 +9,8 @@ vagrant up
 ansible-playbook -i environments/test/inventory playbooks/monitoring_hub.yml -l eye
 ansible-playbook -i environments/test/inventory playbooks/monitoring_node.yml -l monitored
 
+# ensure UI is available
 wait_service "http://$vmbox1/login" "Grafana"
 wait_service "http://$vmbox1:81" "Graphite Browser"
+
+# TODO ensure graphite to ensure statsd metrics from 'beacon1' monitoring_node are present
